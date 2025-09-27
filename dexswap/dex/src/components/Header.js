4ee2874727ba1a7a5a797f882330f732d26f5df7
Rishabh Gupta <rishabhgupta78470@gmail.com>
2025-09-27 14:53:42 +0530

    update Header.js

--- dexswap/dex/src/components/Header.js
+++ dexswap/dex/src/components/Header.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import Logo from "../DexSwap_logo.png";
+import Eth from "../eth.svg";
+
+
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="logo" className="logo" />
+        <div className="headerItem">Swap</div>
+        <div className="headerItem">Tokens</div>
+      </div>
+
+      <div className="rightH">
+        <div className="headerItem">
+        <img src={Eth} alt="eth" className="eth" />
+        Ethereum
+    </div>
+
+    <div className="connectButton" >
+      Connect
+    </div>
+    
+      </div>
+    </header>
   )
 }
 
