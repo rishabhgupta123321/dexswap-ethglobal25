5ce37f4af69c4822a0a097dbb1b137d805cb3381
Rishabh Gupta <rishabhgupta78470@gmail.com>
2025-09-28 03:11:43 +0530

    Ratio set with modified swap.js file

--- dexswap/dex/src/components/Swap.js
+++ dexswap/dex/src/components/Swap.js
@@ -6,6 +6,7 @@ import {
   SettingOutlined,
 } from "@ant-design/icons";
 import tokenList from "../tokenList.json";
+import axios from "axios";
 
 function Swap() {
   const [slippage, setSlippage] = useState(2.5);
@@ -15,6 +16,8 @@ function Swap() {
   const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
   const [isOpen, setIsOpen] = useState(false);
   const [changeToken, setChangeToken] = useState(1);
+  const [prices, setPrices] = useState(null);
+
 
   function handleSlippageChange(e) {
     setSlippage(e.target.value);
@@ -22,6 +25,11 @@ function Swap() {
 
   function changeAmount(e) {
     setTokenOneAmount(e.target.value);
+    if(e.target.value && prices){
+      setTokenTwoAmount((e.target.value * prices.ratio).toFixed(2))
+    }else{
+      setTokenTwoAmount(null);
+    }
   }
 
   function switchTokens() {
@@ -44,6 +52,23 @@ function Swap() {
     }
     setIsOpen(false);
   }
+
+async function fetchPrices(one, two){
+
+      const res = await axios.get(`http://localhost:3001/tokenPrice`, {
+        params: {addressOne: one, addressTwo: two}
+      })
+
+      console.log(res.data);
+      setPrices(res.data)
+  }
+
+  useEffect(()=>{
+
+    fetchPrices(tokenList[0].address, tokenList[1].address)
+
+  }, [])
+
 
   const settings = (
     <>
