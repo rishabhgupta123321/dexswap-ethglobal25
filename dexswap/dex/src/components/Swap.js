f70d2744469e32f2a7f16f57e47122aa15529539
Rishabh Gupta <rishabhgupta78470@gmail.com>
2025-09-27 20:43:00 +0530

    Added swapping Button in between both box of currency

--- dexswap/dex/src/components/Swap.js
+++ dexswap/dex/src/components/Swap.js
@@ -22,6 +22,17 @@ function Swap() {
     setTokenOneAmount(e.target.value);
   }
 
+  function switchTokens() {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
+    fetchPrices(two.address, one.address);
+  }
+
   const settings = (
     <>
       <div>Slippage Tolerance</div>
@@ -51,6 +62,11 @@ function Swap() {
       <div className="inputs">
         <Input placeholder="0" value={tokenOneAmount} onChange={changeAmount} />
         <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
+
+        <div className="switchButton" onClick={switchTokens}>
+          <ArrowDownOutlined className="switchArrow" />
+        </div>
+
         <div className="assetOne">
           <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
           {tokenOne.ticker}
