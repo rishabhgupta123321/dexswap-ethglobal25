018d531f6dc2c48d2b211a501fefa46715950554
Rishabh Gupta <rishabhgupta78470@gmail.com>
2025-09-28 02:32:59 +0530

    modified index.js file from backend folder dexBack

--- dexswap/dexBack/index.js
+++ dexswap/dexBack/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/tokenPrice", async (req, res) => {
-  return res.status(200).json(usdPrices);
+  return res.status(200).json({});
 });
 
 Moralis.start({
