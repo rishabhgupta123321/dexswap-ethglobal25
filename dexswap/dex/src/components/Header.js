d17678d8c3cacd8a5398b93375af63e1c300b78a
Rishabh Gupta <rishabhgupta78470@gmail.com>
2025-09-28 03:48:21 +0530

    Header.js file updated to connect metamask

--- dexswap/dex/src/components/Header.js
+++ dexswap/dex/src/components/Header.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import Logo from "../DexSwap_logo.png";
+import React from "react";
+import Logo from "../moralis-logo.svg";
 import Eth from "../eth.svg";
 import { Link } from "react-router-dom";
 
+function Header(props) {
 
-function Header() {
+  const {address, isConnected, connect} = props;
+
   return (
     <header>
       <div className="leftH">
         <img src={Logo} alt="logo" className="logo" />
-        
         <Link to="/" className="link">
           <div className="headerItem">Swap</div>
         </Link>
@@ -22,12 +23,12 @@ function Header() {
           <img src={Eth} alt="eth" className="eth" />
           Ethereum
         </div>
-        <div className="connectButton">
-      Connect
+        <div className="connectButton" onClick={connect}>
+          {isConnected ? (address.slice(0,4) +"..." +address.slice(38)) : "Connect"}
         </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
